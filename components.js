cabb5cf53731cbfce48477167c7955185a1f7a69
DmtrBrsn <organon666@gmail.com>
2023-02-18 21:54:00 +0300

    small fix

--- components.js
+++ components.js
@@ -1,23 +1,24 @@
 import * as L from './modules/leaflet/leaflet-src.esm.js';
 
 export function createMapWithWorkoutRoute(workoutData, appendTo) {
-  const garminLatLongToNormal = (gl)=>gl/11930465;
-  const polylinePoints = [];
+  const garminCoorToNormal = (gl)=>gl/11930465;
 
+  const polylinePoints = [];
   for (let rec of workoutData.recordMesgs) {
     if (!rec.hasOwnProperty('positionLat')) continue;
-    polylinePoints.push([garminLatLongToNormal(rec.positionLat),garminLatLongToNormal(rec.positionLong)]);
+    polylinePoints.push([garminCoorToNormal(rec.positionLat),garminCoorToNormal(rec.positionLong)]);
   }
 
   if (polylinePoints.length===0) return;
 
   const startCoordinates = polylinePoints[0];
+  const startZoom = 7;
 
   let mapElement = document.createElement('div');
   mapElement.id=`map-${workoutData.id_workouts}`;
   mapElement.style.height = '180px';
   appendTo.appendChild(mapElement);
-  var map = L.map(mapElement.id).setView(startCoordinates, 7);
+  var map = L.map(mapElement.id).setView(startCoordinates, startZoom);
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
